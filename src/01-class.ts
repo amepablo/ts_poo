5578d6e299717970edbbcb867b51b7370ba7f245
Pablo <amepablo@gmail.com>
2022-07-18 12:12:00 -0300

    Clase 2: Class

---
+++ src/01-class.ts
@@ -0,0 +1,31 @@
+// Uso de clases
+const date = new Date();
+date.getHours();
+date.getTime();
+date.toISOString();
+
+const date2 = new Date(1986, 4, 26); // 0 enero 11 dic
+date2.getHours();
+date2.getTime();
+date2.toISOString();
+
+console.log(date);
+console.log(date2);
+
+
+// Crear una clase
+class MyDate {
+  year: number;
+  month: number;
+  day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+}
+
+// Creación y ejecución de una instancia
+const myDate = new MyDate(1986, 4, 26);
+console.log(myDate);
