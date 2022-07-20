9d3a3e22a42460bcbd0a1771bb347a48a749f05d
Pablo <amepablo@gmail.com>
2022-07-20 12:16:04 -0300

    Clase 10: Acceso protegido

---
+++ src/09-protected.ts
@@ -0,0 +1,42 @@
+export class Animal {
+  constructor(protected name: string) {}
+
+  move() {
+    console.log('Moving along!!');
+  }
+
+  greeting() {
+    return `Hello I'm ${this.name}`;
+  }
+
+  protected doSomething() {
+    console.log('doooo');
+  }
+}
+
+export class Dog extends Animal {
+
+  constructor(
+    name: string,
+    public owner: string
+  ) {
+    super(name);
+  }
+
+  woof(times: number): void {
+    for (let index = 0; index < times; index++) {
+      console.log(`wooof! ${this.name}`);
+    }
+    this.doSomething();
+  }
+
+  move() {
+    console.log('moving as a dog');
+    super.move();
+  }
+}
+
+const cheis = new Dog('cheis', 'Pablo');
+// cheis.name = 'otro nombre';
+cheis.woof(1);
+cheis.move();
