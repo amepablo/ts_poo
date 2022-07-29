3a2282ec25ed916b97d226ae1a304dd81cce1e3e
Pablo <amepablo@gmail.com>
2022-07-29 16:53:24 -0300

    Clase 24: decoradores

---
+++ src/app/dtos/category.dto.ts
@@ -0,0 +1,29 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsUrl, Length, validateOrReject } from 'class-validator'
+
+import { AccessType, Category } from "../models/category.model";
+
+export interface ICreateCategoryDto extends Omit<Category, 'id' >{}
+export class CreateCategoryDto implements ICreateCategoryDto {
+
+  @IsNotEmpty()
+  @Length(4, 140)
+  name!: string;
+
+  @IsUrl()
+  @IsNotEmpty()
+  image!: string;
+
+  @IsOptional()
+  @IsEnum(AccessType)
+  access?: AccessType | undefined;
+}
+
+(async ()=>{
+  try {
+    const dto = new CreateCategoryDto();
+    dto.name = 'a';
+    await validateOrReject(dto)
+  } catch (error) {
+    console.log(error)
+  }
+})()
