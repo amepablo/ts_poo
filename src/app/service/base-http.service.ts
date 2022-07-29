a40e4140a366837adcf1c355f1a0b4cf50b8eaad
Pablo <amepablo@gmail.com>
2022-07-29 16:21:30 -0300

    Clase 22: Generics en métodos

--- src/app/service/base-http.service.ts
+++ src/app/service/base-http.service.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { UpdateProductDto } from '../dtos/product.dto';
 import { Product } from '../models/product.model';
 import { Category } from './../models/category.model'
 
@@ -6,13 +7,19 @@ export class BaseHttpService<TypeClass> {
   // data: TypeClass[] = [];
 
   constructor(
-    private url: string
+    protected url: string
   ) {}
 
   async getAll() {
     const { data } = await axios.get<Product[]>(this.url);
     return data;
   }
+
+  async update<ID, DTO>(id: ID, changes: DTO) {
+    const { data } = await axios.put(`${this.url}/${id}`, changes);
+    return data;
+  }
+
 }
 
 // const service = new BaseHttpService<string>();
@@ -27,6 +34,9 @@ export class BaseHttpService<TypeClass> {
   const productService = new BaseHttpService<Product>(url1);
   const rta = await productService.getAll();
   console.log('products', rta.length);
+  productService.update<Product['id'], UpdateProductDto>(2, {
+    title:'asa',
+  });
 
   const url2 = 'https://api.escuelajs.co/api/v1/categories';
   const categoryService = new BaseHttpService<Category>(url2)
